124b0d43e45244c309d27936947de40ed2d896fb
jaeguin <jaeguin200@gmail.com>
2024-05-18 21:37:57 +0900

    props

--- src/App.js
+++ src/App.js
@@ -2,23 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const topics = [
+    {id:1, title:'aa', body:'aa is aa'},
+    {id:2, title:'bb', body:'bb is bb'},
+    {id:3, title:'cc', body:'cc is cc'}
+  ]
   return (
     <div className="App">
-      <Header></Header>
-      <Article></Article>
+      <Header title="react study with jaeguin"></Header>
+      <Article topics={topics}></Article>
     </div>
   );
 }
-function Header(){
+function Header(props){
+  console.log('props', props, props.title)
   return <header>
-    <h1><a href='/'>JAEGUIN</a>    </h1>
+    <h1><a href='/'>{props.title}</a>    </h1>
   </header>
 }
-function Article(){
+function Article(props){
+  const lis = []
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+  }
   return <article>
-    <div className="App">
+    <div>
         <p>
-          Edit React Code with Jaeguin
+          {props.title}
         </p>
         <img src={logo} className="App-logo" alt="logo" />
         
@@ -28,8 +39,11 @@ function Article(){
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi Jaeguin!
+          {props.body}
         </a>
+        <p>
+          {lis}
+        </p>
     </div>
   </article>
 }
