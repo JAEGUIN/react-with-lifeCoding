59e9d4b376cb035ca580cb149e91d9353f6db741
jaeguin <jaeguin200@gmail.com>
2024-05-25 19:03:14 +0900

    create

--- src/App.js
+++ src/App.js
@@ -33,6 +33,21 @@ function Nav(props){
     </ol>
   </nav>
 }
+function Create(props){
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type='text' name='title' placeholder='title'/></p>
+      <p><textarea type='text' name='body' placeholder='body'/></p>
+      <p><input type='submit' value='Create'/></p>
+    </form>
+  </article>
+}
 function App() {
   // const _mode = useState('welcome');
   // console.log('_mode : ',_mode);
@@ -41,12 +56,13 @@ function App() {
   //setMode로 앱 컴포넌트가 다시 실행됨
   const[mode, setMode] = useState('welcome');
   const[id, setId] = useState(null);
-  const topics = [
+  const[nextId, setNextId] = useState(5);
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'},
     {id:4, title:'jaeguin', body:'jaeguin is ...'}
-  ] 
+  ]);
   let content = null;
   if(mode === 'welcome'){
     content = <Article title='welcome' body='hi jaeguin'></Article>
@@ -60,6 +76,16 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>
+  }else if(mode === 'create'){
+    content = <Create onCreate={(title, body)=>{
+      const newTopic = {id:nextId, title:title, body:body}
+      const newTopics = [...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('read');
+      setId(nextId);
+      setNextId(nextId+1);
+    }}></Create>
   }
   return (
     <div>
@@ -74,6 +100,11 @@ function App() {
       }}></Nav>
       {/* <Article title="Welcome" body="Hello, JAEGUIN"></Article> */}
       {content}
+      <br/>
+      <a href='/create' onClick={event=>{
+        event.preventDefault();
+        setMode('create');
+      }}>Create</a>
     </div>
   );
 }
