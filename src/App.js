386fa2e971a1e13654ba66801e53cee5b448d4c1
jaeguin <jaeguin200@gmail.com>
2024-06-04 23:53:46 +0900

    update

--- src/App.js
+++ src/App.js
@@ -42,9 +42,30 @@ function Create(props){
       const body = event.target.body.value;
       props.onCreate(title, body);
     }}>
-      <p><input type='text' name='title' placeholder='title'/></p>
-      <p><textarea type='text' name='body' placeholder='body'/></p>
-      <p><input type='submit' value='Create'/></p>
+      <p><input type="text" name="title" placeholder="title"/></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="Create"></input></p>
+    </form>
+  </article>
+}
+function Update(props){
+  const [title, setTitle] = useState(props.title);
+  const [body, setBody] = useState(props.body);
+  return <article>
+    <h2>Update</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onUpdate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder="title" value={title} onChange={event=>{
+        setTitle(event.target.value);
+      }}/></p>
+      <p><textarea name="body" placeholder="body" value={body} onChange={event=>{
+        setBody(event.target.value);
+      }}></textarea></p>
+      <p><input type="submit" value="Update"></input></p>
     </form>
   </article>
 }
@@ -64,6 +85,7 @@ function App() {
     {id:4, title:'jaeguin', body:'jaeguin is ...'}
   ]);
   let content = null;
+  let contextController = null;
   if(mode === 'welcome'){
     content = <Article title='welcome' body='hi jaeguin'></Article>
   }else if(mode === 'read'){
@@ -76,6 +98,10 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>
+    contextController = <li><a href={'/update/'+id} onClick={event=>{
+      event.preventDefault();
+      setMode('update');
+    }}>Update</a></li>
   }else if(mode === 'create'){
     content = <Create onCreate={(title, body)=>{
       const newTopic = {id:nextId, title:title, body:body}
@@ -86,6 +112,27 @@ function App() {
       setId(nextId);
       setNextId(nextId+1);
     }}></Create>
+  }else if(mode === 'update'){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++){
+      console.log(topics[i].id, id);
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Update title={title} body={body} onUpdate={(title, body)=>{
+      const newTopics = [...topics]
+      const updatedTopic = {id:id, title:title, body:body}
+      for(let i=0; i<newTopics.length; i++){
+        if(newTopics[i].id === id){
+          newTopics[i] = updatedTopic;
+          break;
+        }
+      }
+      setTopics(newTopics);
+      setMode('read');
+    }}></Update>
   }
   return (
     <div>
@@ -101,10 +148,13 @@ function App() {
       {/* <Article title="Welcome" body="Hello, JAEGUIN"></Article> */}
       {content}
       <br/>
-      <a href='/create' onClick={event=>{
-        event.preventDefault();
-        setMode('create');
-      }}>Create</a>
+      <ul>
+        <li><a href='/create' onClick={event=>{
+          event.preventDefault();
+          setMode('create');
+        }}>Create</a></li>
+        {contextController}
+      </ul>
     </div>
   );
 }
